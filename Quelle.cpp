de091f03d9266c0edc588a83d6c54d257dd91b41
Zouhdi El Abouti <elzouhdiabouti@web.de>
2021-01-09 15:28:39 +0100

    Linker

--- Quelle.cpp
+++ Quelle.cpp
@@ -2,7 +2,16 @@
 #include"Simple_window.h"
 
 
-// https://bumpyroadtocode.com/2017/08/05/how-to-install-and-use-fltk-1-3-4-in-visual-studio-2017-complete-guide/
+
+/*
+fltkd.lib
+wsock32.lib
+comctl32.lib
+fltkjpegd.lib
+fltkimagesd.lib
+%(AdditionalDependencies)
+
+*/
 
 int main() {
 
