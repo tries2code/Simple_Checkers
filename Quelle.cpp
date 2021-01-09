93852d4d62afff65a322d0da11d8d5a74199d982
Zouhdi El Abouti <elzouhdiabouti@web.de>
2021-01-09 15:05:36 +0100

    Link

--- Quelle.cpp
+++ Quelle.cpp
@@ -1,6 +1,9 @@
 #include"Graph.h"
 #include"Simple_window.h"
 
+
+// https://bumpyroadtocode.com/2017/08/05/how-to-install-and-use-fltk-1-3-4-in-visual-studio-2017-complete-guide/
+
 int main() {
 
 	Simple_window win{ {100,100},800,600,"Test Fenster" };
