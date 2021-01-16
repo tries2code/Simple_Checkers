3519b24e1523391de3e23f5e0a5245b5b8ab8569
Zouhdi El Abouti <elzouhdiabouti@web.de>
2021-01-16 22:03:02 +0100

    Regelkonformes Bewegen, noch kein Fangen, noch keine Dame

--- Quelle.cpp
+++ Quelle.cpp
@@ -4,22 +4,46 @@
 //Button unter schwarzen Felder eingefügt, die geben Position wieder
 //Spielsteine richtig angeordnet
 //Spielsteine können ausgewählt und manipulirt werden 
-//field_pressed ist für die Spielmechanik, zurzeit im Aufbau
+//tile_pressed ist für die Spielmechanik, zurzeit im Aufbau
 //Group Klasse mit ls und us ersetzt
 
-int sz = 50;									//Feld Länge und Breite; vorübergehend global
-int ca = sz / 2;									//Anpassung für Kreise, da die sonst oben links auf einem Feld stehen; vorübergehend global
-int ra = 20;									//Radius der Spielsteine; vorübergehend global
 
-int ls = 100;									//Linker Abstand zum Bildschirmrand
-int us = 100;									//Oberer Absatnd zum Bilschirmrand
+//Staus Spielmechanik:
+//Spieler 1 und 2 können nur abwechselnd spielen
+//Spielsteine können nicht rückwärts gehen
+//Spielsteine können nur ein Feld weit laufen
+//oben genanntes kann natürlich auf Grund von bisher nicht gefundenen Bugs gelogen sein
 
-//Helper Function
-Point get_point(int x, int y) {
+//Es fehlt noch:
+//Mechanik zum gegenerische Steine fangen
+//Mechanik für Multikills
+//Trasformation eines Spielsteins zur Dame mit entsprechenden Attributen
+//Tests Tests Tests....
+
+
+
+constexpr int sz = 50;									//Feld Länge und Breite; vorübergehend global
+constexpr int ca = sz / 2;								//Anpassung für Kreise, da die sonst oben links auf einem Feld stehen; vorübergehend global
+constexpr int ra = 20;									//Radius der Spielsteine; vorübergehend global
+
+constexpr int ls = 100;									//Linker Abstand zum Bildschirmrand
+constexpr int us = 100;									//Oberer Absatnd zum Bilschirmrand
+
+const Color c_player1 = Color::red;
+const Color c_player2 = Color::yellow;
+Color c_turn = c_player1;								//Der Spieler der grade dran ist
+
+Graph_lib::Circle* curr_stone = nullptr;
+
+//Helfer Funktionen
+Point get_point(int x, int y) {							//Ermittelt Koordinaten für Spielfeld
 	Point res{ x - x % sz,y - y % sz };
 	return res;
-}		//Ermittelt Koordinaten für Spielfeld
-
+}
+bool operator==(const Graph_lib::Color& a, const Graph_lib::Color& b) {				//Vergleicht Farben
+	if (a.as_int() == b.as_int())return true;
+	return false;
+}
 
 
 
@@ -27,22 +51,26 @@ Point get_point(int x, int y) {
 class My_window : public Window {
 
 	bool button_pushed;
+	bool stone_selected;								//Wurde ein Stein ausgewählt?
 	Button next_button;
 	Button quit_button;
 
-	Vector_ref<Button>field_buttons;
+	Out_box current_turn;								//Sagt einem wer grad dran ist, ja, ist hässlich, kann man überarbeiten
+
+	Vector_ref<Button>field_buttons;					//Für die Knöppe unter den schwarzen Feldern
 	Vector_ref< Graph_lib::Rectangle>checkers;			//Für die Felder
-	Vector_ref< Graph_lib::Circle>stones;
+	Vector_ref< Graph_lib::Circle>stones;				//Für die Spielsteine, sind nur ausgemalte Kreise
 
 public:
 	My_window(Point xy, int w, int h, const string& title) :Window(w, h, title),
-		button_pushed(false),
+		button_pushed(false), stone_selected(false),
 		next_button(Point(x_max() - 70, 0), 70, 20, "Next", cb_next),
-		quit_button(Point(x_max() - 70, 30), 70, 20, "Quit", cb_quit)
+		quit_button(Point(x_max() - 70, 30), 70, 20, "Quit", cb_quit),
+		current_turn(Point{ x_max() - 350, 100 }, 100, 30, "Current Player: ")
 	{
 		for (int x = 0; x < 8; x++) {					//Knöppe unter den Feldern
 			for (int y = 0; y < 8; y++) {
-				if ((y + x) % 2 == 0)field_buttons.push_back(new Button{ {ls + x * sz,us + y * sz},sz,sz ," ",cb_field_pressed });
+				if ((y + x) % 2 == 0)field_buttons.push_back(new Button{ {ls + x * sz,us + y * sz},sz,sz ," ",cb_tile_pressed });
 				attach(field_buttons[field_buttons.size() - 1]);
 			}
 		}
@@ -60,13 +88,14 @@ public:
 				if (y == 1)y = 5;
 			}
 		}
-		for (int i = 0; i < stones.size(); i++) {			//Farbe der Steine, die Kondition if ((y + x) % 2 == 0) im oberen Loop hat bei der Einfärbung irgendwie zu Schwulitäten geführt...keine Ahnung warum
-			if (i % 2 == 0)stones[i].set_fill_color(FL_RED);
-			else stones[i].set_fill_color(FL_YELLOW);
+		for (int i = 0; i < stones.size(); i++) {		//Farbe der Steine, die Kondition if ((y + x) % 2 == 0) im oberen Steine-Loop hat bei der Einfärbung irgendwie zu Schwulitäten geführt...keine Ahnung warum
+			if (i % 2 == 0)stones[i].set_fill_color(c_player2);
+			else stones[i].set_fill_color(c_player1);
 		}
-
 		attach(next_button);
 		attach(quit_button);
+		attach(current_turn);
+		current_turn.put("Red");						//rot fängt an
 	}
 	void wait_for_button() {
 		while (!button_pushed) Fl::wait();
@@ -75,8 +104,14 @@ public:
 	}
 
 private:
+	bool tile_empty(Point p) {
+		Point curr = { p.x + ca,p.y + ca };
+		for (int i = 0; i < stones.size(); i++) {
+			if (stones[i].center() == curr)return false;
+		}
+		return true;
+	}
 	Graph_lib::Circle* get_stone(Point p) {				//Ermittelt Spielstein
-
 		Graph_lib::Circle* res;
 		Point curr;
 		for (int i = 0; i < stones.size(); i++) {
@@ -92,33 +127,55 @@ private:
 
 	static void cb_next(Address, Address addr) { static_cast<My_window*>(addr)->next(); }		//Sogenannte Call Back Funktionen für Knöpfe, callen die eigentlichen Funktionen für die Knöpfe
 	static void cb_quit(Address, Address addr) { reference_to<My_window>(addr).quit(); }		//Ist die Stroustrup Variante, macht auch nur 		return *static_cast<W*>(pw);
-	static void cb_field_pressed(Address, Address addr) { reference_to<My_window>(addr).field_pressed(); }
+	static void cb_tile_pressed(Address, Address addr) { reference_to<My_window>(addr).tile_pressed(); }
 
-	void next() { button_pushed = true; }				//Löst FL::redraw() (in void wait_for_button()) aus.
+	void next() { button_pushed = true; }								//Löst FL::redraw() (in void wait_for_button()) aus.
 	void quit() { hide(); button_pushed = true; }
 
-	void  field_pressed() {
-		Point p = get_point(Fl::event_x(), Fl::event_y());
+	void  tile_pressed() {
+		Point p = get_point(Fl::event_x(), Fl::event_y());				//p=Koordinaten der aktuell gedrücketen Feldes
 
-		Graph_lib::Circle* c = get_stone(p);
+		if (!tile_empty(p) && !stone_selected) {
+			curr_stone = get_stone(p);
+			Color c_curr = curr_stone->fill_color();
+			if (c_curr == c_turn) {
+				curr_stone->set_color(Color::cyan);
+				stone_selected = true;
+			}
+		}
 
-		c->set_color(Color::cyan);
-		c->move(-sz, -sz);
-		cout << p.x << " " << p.y << endl;
+		if (tile_empty(p) && stone_selected) {
+			Point pp = curr_stone->center();
+			Point diff = { p.x - pp.x,p.y - pp.y };
+
+			if (c_turn == c_player1 && p.y < pp.y && abs(diff.x) <= sz + ca && abs(diff.y) <= sz + ca && diff.y + ca != 0) {					//Bewegung rot
+				curr_stone->move(diff.x + ca, diff.y + ca);
+				curr_stone->set_color(Color::black);
+				curr_stone = nullptr;
+				stone_selected = false;
+				c_turn = c_player2;
+				current_turn.put("Yellow");
+			}
+
+			if (c_turn == c_player2 && p.y > pp.y && abs(diff.x) <= sz + ca && abs(diff.y) <= sz - ca) {	//Bewegung gelb
+				curr_stone->move(diff.x + ca, diff.y + ca);
+				curr_stone->set_color(Color::black);
+				curr_stone = nullptr;
+				stone_selected = false;
+				c_turn = c_player1;
+				current_turn.put("Red");
+			}
+		}
+		cout << p.x << " " << p.y << endl;					//Ist nur fürs debuggen
 		Fl::redraw();
 	}
 };
 
-
-
 int main() {
 
 	//My_window win{ {100,100},x_max(),y_max(),"Schach oder Dame" };
-	My_window win{ {100,100},1000,800,"Schach oder Dame" };
-
-
+	My_window win{ {100,100},1000,800,"Schach oder Dame" };						//Fenster zum Debuggen verkleinert um das Konsolenfenster zu sehen
 
 
 	win.wait_for_button();
-
 }
